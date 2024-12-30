6a92f5fdeabb14264f7cb94da44a6323446845fa
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-12-30 04:36:03 +0000

    unicafe step 4 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,28 +10,52 @@ const Button = ({onClick,text}) =>  {
  )
 }
 
-const App = () => {
+//Refactored Statistic Component
+const Statistics = ({ allFeedback }) => {
+  const { good, neutral, bad } = allFeedback; // Destructure the properties
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>All: {total}</p>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Average: {(good - bad) / total}</p>
+        <p>Positive: {(good / total) * 100} %</p>
+      </div>
+    );
+  }
+};
+
+function App() {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const allFeedback = { good, neutral, bad }
+  
 
   return (
     <div>
       <h2>give feedback</h2>
       <br />
       <Button onClick={() => setGood(good + 1)} text={'Good'} />
-      <Button onClick={() => setNeutral(neutral  + 1)} text={'Neutral'} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={'Neutral'} />
       <Button onClick={() => setBad(bad + 1)} text={'Bad'} />
       <br />
-      <h2>statistics </h2>
-      <p>All: {good + neutral + bad}</p>
-      <p>Good: {good} </p>
-      <p>Neutral: {neutral} </p>
-      <p>Bad: {bad} </p>
-      <p>Average: {(good - bad)/ (good + neutral + bad)}</p>
-      <p>Positive: {(good)/(good + neutral + bad)} %</p>
-
+      <Statistics allFeedback={allFeedback} />
+      
 
     </div>
   )
