41e3301661cc9cf837f0401a89fd37ce01a39d2e
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-12-30 03:46:49 +0000

    unicafe step 1 complete

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+
+//button to increase ratings
+
+const Button = ({onClick,text}) =>  {
+ return( <button onClick={onClick} >
+    {text}
+ </button>
+ )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <br />
+      <Button onClick={() => setGood(good + 1)} text={'Good'} />
+      <Button onClick={() => setNeutral(neutral  + 1)} text={'Neutral'} />
+      <Button onClick={() => setBad(bad + 1)} text={'Bad'} />
+      <br />
+      <br />
+      
+      <p>Good: {good} </p>
+      <p>Neutral: {neutral} </p>
+      <p>Bad: {bad} </p>
+
+    </div>
+  )
+}
+
+export default App
